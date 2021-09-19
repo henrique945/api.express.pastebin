0681c61876a312347953ab864b8592c731eef3a3
henrique945 <henrique.rodrigues7@hotmail.com>
2021-09-19 17:54:46 -0300

    create(server.js): start an express server

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+app.set('view engine', 'ejs')
+app.set()
+
+app.get('/', (req, res) => {
+	const code = `Bem vindo ao Pastebin!
+	
+Use o menu a sua direita para criar novos arquivos 
+e compartilhar com seus amigos.`
+
+	res.render('home', { code })
+})
+
+app.listen(3000)
