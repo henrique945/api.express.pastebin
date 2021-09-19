7e1d71a4a6b750d9fed2660f140ae573d719c222
henrique945 <henrique.rodrigues7@hotmail.com>
2021-09-19 18:37:16 -0300

    update(server.js): add moongose lib and connect with MongoDB

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+
+const Document = require('./models/Document')
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/pastebin')
 
 app.get('/', (req, res) => {
 	const code = `Bem vindo ao Pastebin!
@@ -14,6 +19,16 @@ e compartilhar com seus amigos.`
 
 app.get('/new', (req, res) => {
 	res.render('new')
+})
+
+app.post('/save', async (req, res) => {
+	const value = req.body.value;
+	try {
+		const document = await Document.create({ value })
+		res.redirect(`/${document.id}`)
+	} catch (e) {
+		res.render('new', { value })
+	}
 })
 
 app.listen(3000)
